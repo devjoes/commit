4539ee0fec6148c0af55e6991eff0cbc8dd79cee
devjoes <39987252+devjoes@users.noreply.github.com>
2024-12-20 11:26:07 +0000

    debug: more logging

--- lib/github-client.ts
+++ lib/github-client.ts
@@ -1,15 +1,19 @@
+import * as core from "@actions/core";
 import axios from "axios";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require("../package.json");
 
+core.info("pkg: " + pkg);
+core.info("pkg.name: " + pkg.name);
+
 const github = axios.create({
-  baseURL: `https://api.github.com/`,
-  headers: {
-    accept: `application/vnd.github.v3+json`,
-    authorization: `bearer ${process.env.GH_TOKEN}`,
-    "user-agent": `${pkg.name}/${pkg.version}`,
-  },
+	baseURL: `https://api.github.com/`,
+	headers: {
+		accept: `application/vnd.github.v3+json`,
+		authorization: `bearer ${process.env.GH_TOKEN}`,
+		"user-agent": `${pkg.name}/${pkg.version}`,
+	},
 });
 
 export default github;
