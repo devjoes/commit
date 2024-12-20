c849965b40951620953ffc9162b356a090d0ab17
devjoes <39987252+devjoes@users.noreply.github.com>
2024-12-20 11:29:23 +0000

    debug: more logging

--- index.ts
+++ index.ts
@@ -7,55 +7,52 @@ import { getBlobsFromFiles } from "./lib/blob";
 import { Tree } from "./lib/tree";
 import { Commit } from "./lib/commit";
 
+core.debug("index.ts");
 export default async function run(): Promise<void> {
-  try {
-    // Get repo
-    core.debug('GITHUB_REPOSITORY:' + process.env.GITHUB_REPOSITORY);
-    const repo = new Repo(process.env.GITHUB_REPOSITORY);
-    await repo.load();
-    core.debug('repo loaded');
+	core.debug("index.ts run");
+	try {
+		// Get repo
+		core.debug("GITHUB_REPOSITORY:" + process.env.GITHUB_REPOSITORY);
+		const repo = new Repo(process.env.GITHUB_REPOSITORY);
+		await repo.load();
+		core.debug("repo loaded");
 
-    // Get inputs
-    const files = getInput("files");
-    const baseDir = getInput("workspace", {
-      default: process.env.GITHUB_WORKSPACE,
-    });
-    const commitMessage = getInput("commit-message");
+		// Get inputs
+		const files = getInput("files");
+		const baseDir = getInput("workspace", {
+			default: process.env.GITHUB_WORKSPACE,
+		});
+		const commitMessage = getInput("commit-message");
 
-    // Load ref details
-    const ref = new Ref(
-      repo,
-      getInput("ref", { default: repo.defaultBranchRef })
-    );
-    
-    await ref.load();
-    core.debug('ref loaded');
+		// Load ref details
+		const ref = new Ref(repo, getInput("ref", { default: repo.defaultBranchRef }));
 
-    // Expand files to an array of "blobs", which will be created on GitHub via the create blob API
-    const blobs = getBlobsFromFiles(repo, files, { baseDir });
-    core.debug(
-      `Received ${blobs.length} blob${
-        blobs.length === 1 ? "" : "s"
-      }: ${blobs.map((blob) => blob.absoluteFilePath).join(", ")}`
-    );
+		await ref.load();
+		core.debug("ref loaded");
 
-    // Create a tree
-    const tree: Tree = new Tree(repo, blobs, ref.treeOid);
+		// Expand files to an array of "blobs", which will be created on GitHub via the create blob API
+		const blobs = getBlobsFromFiles(repo, files, { baseDir });
+		core.debug(
+			`Received ${blobs.length} blob${blobs.length === 1 ? "" : "s"}: ${blobs
+				.map((blob) => blob.absoluteFilePath)
+				.join(", ")}`
+		);
 
-    // Create commit
-    const commit: Commit = new Commit(repo, tree, commitMessage, [
-      ref.commitOid,
-    ]);
-    await commit.save();
+		// Create a tree
+		const tree: Tree = new Tree(repo, blobs, ref.treeOid);
 
-    // Set commit sha output
-    core.setOutput("commit-sha", commit.sha);
+		// Create commit
+		const commit: Commit = new Commit(repo, tree, commitMessage, [ref.commitOid]);
+		await commit.save();
 
-    // Update ref to point at new commit sha
-    await ref.update(commit.sha);
-  } catch (e) {
-    core.setFailed(e);
-  }
+		// Set commit sha output
+		core.setOutput("commit-sha", commit.sha);
+
+		// Update ref to point at new commit sha
+		await ref.update(commit.sha);
+	} catch (e) {
+		core.setFailed(e);
+	}
 }
 
 run();
